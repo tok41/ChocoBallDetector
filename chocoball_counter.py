a85dcbf19f3a2922f1ae5fa21c94da4df2e63ca6
tok41 <hippy.hikky@gmail.com>
2018-05-26 00:12:40 +0900

    add webapp

--- chocoball_counter.py
+++ chocoball_counter.py
@@ -53,10 +53,11 @@ class ChocoballDetector:
         Returns:
             dict{'box':[[ymin,xmin,ymax,xmax]], 'objects':[object_id], 'scores':[score]}
         """
-        img_pil = Image.open(BytesIO(img))
+        #img_pil = Image.open(BytesIO(img))
+        img_pil = Image.open(img)
         img_arr = np.asarray(img_pil).transpose(2, 0, 1).astype(np.float32)
         bboxes, labels, scores = self.model_frcnn.predict([img_arr])
-        return {'box': bboxes, 'objects': labels, 'scores': scores}
+        return {'box': bboxes[0], 'objects': labels[0], 'scores': scores[0]}
 
     def detectChocoballImage(self, img):
         """
@@ -67,17 +68,18 @@ class ChocoballDetector:
         Returns:
             dict{'img':image_binary, 'box':[[ymin,xmin,ymax,xmax]], 'objects':[object_id], 'scores':[score]}
         """
-        img_pil = Image.open(BytesIO(img))
+        # img_pil = Image.open(BytesIO(img))
+        img_pil = Image.open(img)
         img_arr = np.asarray(img_pil).transpose(2, 0, 1).astype(np.float32)
         bboxes, labels, scores = self.model_frcnn.predict([img_arr])
 
         fig = plt.figure(figsize=(5, 4))
         ax = fig.subplots(1, 1)
         vis_bbox(img_arr, bboxes[0], labels[0], ax=ax)
-        plt.savefig('tmp.png')
-        with open('tmp.png', 'rb') as f:
+        plt.savefig('tmp/tmp.png')
+        with open('tmp/tmp.png', 'rb') as f:
             img_b = f.read()
-        return {'img': img_b, 'box': bboxes, 'objects': labels, 'scores': scores}
+        return {'img': img_b, 'box': bboxes[0], 'objects': labels[0], 'scores': scores[0]}
 
 
 def main():
@@ -102,20 +104,10 @@ def main():
     # scores = res['scores']
     # classes = cd.classes
 
-    # res['img'].savefig('out/detected.png')
-    print(type(res['img']))
     img_pil = Image.open(BytesIO(res['img']))
     img_pil.show()
+    img_pil.save('out/detected.png')
 
-    # fig = plt.figure(figsize=(12, 4))
-    # img = Image.open(img_file)
-    # img_arr = np.asarray(img).transpose(2, 0, 1).astype(np.float32)
-    # ax = fig.subplots(1, 2)
-    # vis_bbox(img_arr, bboxes[0], labels[0], ax=ax[0])
-    # vis_bbox(img_arr, bboxes[0], labels[0],
-    #          scores[0], label_names=classes, ax=ax[1])
-    # # plt.savefig('out/detected.png')
-    # plt.show()
     print("detected choco-ball : ", np.sum(labels[0] == 0))
 
     return 0
