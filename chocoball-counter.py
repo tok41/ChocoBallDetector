ff07c26ee463a04bc656e8bd27c3d4c640e06ed6
tok41 <hippy.hikky@gmail.com>
2018-05-25 16:49:18 +0900

    update

--- chocoball-counter.py
+++ chocoball-counter.py
@@ -10,10 +10,12 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
+import io
 import os
 import glob
 import numpy as np
 from PIL import Image
+from io import BytesIO
 import random
 
 from chainercv.visualizations import vis_bbox
@@ -43,8 +45,39 @@ class ChocoballDetector:
         return 0
 
     def detectChocoball(self, img):
-        bboxes, labels, scores = self.model_frcnn.predict([img])
+        """
+        ChocoBallの個数をカウントする
+
+        Args:
+            img : jpeg image (binary)
+        Returns:
+            dict{'box':[[ymin,xmin,ymax,xmax]], 'objects':[object_id], 'scores':[score]}
+        """
+        img_pil = Image.open(BytesIO(img))
+        img_arr = np.asarray(img_pil).transpose(2, 0, 1).astype(np.float32)
+        bboxes, labels, scores = self.model_frcnn.predict([img_arr])
         return {'box': bboxes, 'objects': labels, 'scores': scores}
+
+    def detectChocoballImage(self, img):
+        """
+        ChocoBallの個数をカウントする
+
+        Args:
+            img : jpeg image (binary)
+        Returns:
+            dict{'img':image_binary, 'box':[[ymin,xmin,ymax,xmax]], 'objects':[object_id], 'scores':[score]}
+        """
+        img_pil = Image.open(BytesIO(img))
+        img_arr = np.asarray(img_pil).transpose(2, 0, 1).astype(np.float32)
+        bboxes, labels, scores = self.model_frcnn.predict([img_arr])
+
+        fig = plt.figure(figsize=(5, 4))
+        ax = fig.subplots(1, 1)
+        vis_bbox(img_arr, bboxes[0], labels[0], ax=ax)
+        plt.savefig('tmp.png')
+        with open('tmp.png', 'rb') as f:
+            img_b = f.read()
+        return {'img': img_b, 'box': bboxes, 'objects': labels, 'scores': scores}
 
 
 def main():
@@ -56,23 +89,33 @@ def main():
 
     img_file = random.choice(img_files)
     print('image_file : ', img_file)
-    img = Image.open(img_file)
-    img_arr = np.asarray(img).transpose(2, 0, 1).astype(np.float32)
+    with open(img_file, 'rb') as f:
+        img_b = f.read()
 
+    # クラスのインスタンス作成
     cd = ChocoballDetector()
-    res = cd.detectChocoball(img_arr)
+    # チョコボールの計測
+    res = cd.detectChocoballImage(img_b)
 
-    bboxes = res['box']
+    # bboxes = res['box']
     labels = res['objects']
-    scores = res['scores']
-    classes = cd.classes
+    # scores = res['scores']
+    # classes = cd.classes
 
-    fig = plt.figure(figsize=(12, 4))
-    ax = fig.subplots(1, 2)
-    vis_bbox(img_arr, bboxes[0], labels[0], ax=ax[0])
-    vis_bbox(img_arr, bboxes[0], labels[0],
-             scores[0], label_names=classes, ax=ax[1])
-    plt.savefig('out/detected.png')
+    # res['img'].savefig('out/detected.png')
+    print(type(res['img']))
+    img_pil = Image.open(BytesIO(res['img']))
+    img_pil.show()
+
+    # fig = plt.figure(figsize=(12, 4))
+    # img = Image.open(img_file)
+    # img_arr = np.asarray(img).transpose(2, 0, 1).astype(np.float32)
+    # ax = fig.subplots(1, 2)
+    # vis_bbox(img_arr, bboxes[0], labels[0], ax=ax[0])
+    # vis_bbox(img_arr, bboxes[0], labels[0],
+    #          scores[0], label_names=classes, ax=ax[1])
+    # # plt.savefig('out/detected.png')
+    # plt.show()
     print("detected choco-ball : ", np.sum(labels[0] == 0))
 
     return 0
