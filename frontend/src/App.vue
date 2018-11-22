96acc98028eafb124ad2542c69380366b01c671e
masata <masatanish@gmail.com>
2018-11-22 15:55:37 +0900

    add bootstrap-vue and implement page header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,7 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<template lang="pug">
+  #app
+    page-header
+    router-view
 </template>
 
 <style>
@@ -13,7 +9,6 @@
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 #nav {
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,13 @@
+import '@babel/polyfill'
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router'
+import PageHeader from './components/PageHeader.vue'
 
 Vue.config.productionTip = false
+
+Vue.component('page-header', PageHeader)
 
 new Vue({
   router,
