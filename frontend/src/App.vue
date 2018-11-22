40b9e62c62e390c8e85595171db1c39e0b00366c
masata <masatanish@gmail.com>
2018-11-22 17:46:53 +0900

    add gallary page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,6 +2,24 @@
   #app
     page-header
     router-view
+    footer
+      b-navbar(fixed="bottom" variant="dark" type="dark")
+        b-navbar-nav
+          b-nav-text &copy; 2018 Tok41
+
+        //-b-navbar-nav
+          b-nav-item test
+          b-nav-text hoge
+          b-nav-text aho
+        //-b-navbar-nav
+          b-nav-item
+            b-img(src="@/assets/hippyhikky.png" fluid rounded width="36px")
+          b-nav-item
+            b-link(href="https://twitter.com/yoichi_t" fluid rounded)
+              b-img(src="@/assets/twitter.png" width="36px")
+          b-nav-item
+            b-link(href="https://github.com/tok41" fluid rounded)
+              b-img(src="@/assets/github.png" width="36px")
 </template>
 
 <style>
@@ -23,4 +41,5 @@
 #nav a.router-link-exact-active {
   color: #42b983;
 }
+
 </style>
